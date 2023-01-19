ef28dd32612e23c2e899f4175a240503626488dc
abdjahiduddin <abd.jahiduddin@gmail.com>
2023-01-20 06:00:17 +0700

    Setting up ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${process.env.STAGE}.stage.env`],
+      isGlobal: true,
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  const env = configService.get<string>('ENV');
+
   await app.listen(3000);
-  logger.log(`Application listening on port 3000`);
+  logger.log(`${env}, Application listening on port ${port}`);
 }
 bootstrap();
